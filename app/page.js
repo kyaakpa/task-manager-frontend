b05c231bf4018d8262d4c2dc3e016445e18957a1
kyaakpa <100368545+kyaakpa@users.noreply.github.com>
2023-12-17 22:26:59 +1100

    overflow-hidden;

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ import ListTasks from "@/components/ListTasks";
 
 export default function Home() {
   return (
-    <div className="h-[100vh]">
+    <div className="h-[100vh] overflow-hidden">
       <ToastContainer />
       <InputTasks />
       <ListTasks />
